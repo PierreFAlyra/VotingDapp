e333aea24d8f10c306b5741561c8cbba1a94d32e
PierreFAlyra <pft@pika>
2023-06-22 18:00:20 +0200

    Fix DOS hack on tallyVotes function

--- truffle/contracts/Voting.sol
+++ truffle/contracts/Voting.sol
@@ -129,6 +129,10 @@ contract Voting is Ownable {
         voters[msg.sender].hasVoted = true;
         proposalsArray[_id].voteCount++;
 
+        if (proposalsArray[_id].voteCount > proposalsArray[winningProposalID].voteCount) {
+            winningProposalID = _id;
+        }
+
         emit Voted(msg.sender, _id);
     }
 
@@ -171,14 +175,7 @@ contract Voting is Ownable {
     /// stores winningProposalID should be called only when workflowStatus is VotingSessionEnded
    function tallyVotes() external onlyOwner {
        require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");
-       uint _winningProposalId;
-      for (uint256 p = 0; p < proposalsArray.length; p++) {
-           if (proposalsArray[p].voteCount > proposalsArray[_winningProposalId].voteCount) {
-               _winningProposalId = p;
-          }
-       }
-       winningProposalID = _winningProposalId;
-       
+              
        workflowStatus = WorkflowStatus.VotesTallied;
        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
     }
